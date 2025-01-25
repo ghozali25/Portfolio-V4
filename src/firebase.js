7c1c4039d63fce1c79a9597d875dc625f1776ce8
Ali <108579846+ghozali25@users.noreply.github.com>
2025-01-25 13:44:20 +0000

    update

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore"
+import { collection, addDoc, getDocs } from "@firebase/firestore"; // Perbarui ini
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBiRi7UsjeGrg5yQmP2XdC1EqAybyyEjg8",
+  authDomain: "transretail-e7318.firebaseapp.com",
+  projectId: "transretail-e7318",
+  storageBucket: "transretail-e7318.firebasestorage.app",
+  messagingSenderId: "856404642895",
+  appId: "1:856404642895:web:2170bde84fbd0dbe8f6e3a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db, collection, addDoc };
